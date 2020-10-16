106769d757411c000325b38c6a3256b833077bca
Indra Lukmana <indralukmana@live.com>
2020-10-16 19:25:09 +0700

    📦 NEW: Add User entity

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,10 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import path from 'path';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const MikroOrmConfigurations: Parameters<typeof MikroORM.init>[0] = {
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   user: 'postgres',
   password: '',
