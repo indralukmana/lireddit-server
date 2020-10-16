240383dee39f15bc2907b3bac209d75d62d92217
Indra Lukmana <indralukmana@live.com>
2020-10-16 09:53:04 +0700

    📦 NEW: Add express base

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import MikroOrmConfigurations from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(MikroOrmConfigurations);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: '1' });
-  await orm.em.persistAndFlush(post);
-  console.log('======= SQL =======');
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server started at port 4000');
+  });
 };
 
 try {
