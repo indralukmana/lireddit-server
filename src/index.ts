a08cecb016d37a8f7c32cad9a132779e1d623a86
Indra Lukmana <indralukmana@live.com>
2020-10-20 11:02:53 +0700

    📦 NEW: Add logout mutation

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import { buildSchema } from 'type-graphql';
 
-import { __prod__ } from './constants';
+import { SESSION_COOKIE_NAME, __prod__ } from './constants';
 import MikroOrmConfigurations from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
@@ -31,7 +31,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: 'qid',
+      name: SESSION_COOKIE_NAME,
       store: new RedisStore({ client: redisClient, disableTouch: true }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from 'type-graphql';
 import argon2 from 'argon2';
+import { SESSION_COOKIE_NAME } from '../constants';
 
 @InputType()
 class UsernamePasswordInput {
@@ -129,5 +130,20 @@ export class UserResolver {
     req.session.userId = user.id;
 
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext): Promise<Boolean> {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(SESSION_COOKIE_NAME);
+        if (err) {
+          console.log({ err });
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      })
+    );
   }
 }
